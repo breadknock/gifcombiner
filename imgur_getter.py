c5495e22044fe77b0085bb9cf459f3e0dda7815f
breadknock <brenoc@rpi.edu>
2015-04-23 23:13:57 -0400

    Added class structure

--- imgur_getter.py
+++ imgur_getter.py
@@ -5,27 +5,48 @@ import urllib
 import string
 import shutil
 
-s = open('secret.txt','r')
-client_id = s.readline().strip()
-client_secret = s.readline().strip()
-client = ImgurClient(client_id,client_secret)
+class ImgurAPIClient:
+    def __init__(self):
+        secret_data = open('secret.txt','r')
+        client_id = secret_data.readline().strip()
+        client_secret = secret_data.readline().strip()
+        self.client = ImgurClient(client_id, client_secret)
+        self.url_opener = urllib.URLopener()
 
-def id_generator(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+    # Implement error checking
+    def get_album(self,albumID,randID):
+        target_album = self.client.get_album(albumID)
+        links = [i['link'] for i in target_album.images]
+        totalsize = sum([i['size'] for i in target_album.images])
+        for idx, i in enumerate(links):
+            self.url_opener.retrieve(i,randID + '/' + str(idx).zfill(4) + '.gif')
+        return True
 
-def parseAlbum(albumID):
-    al = client.get_album(albumID)
-    links = [i['link'] for i in al.images]
-    rand_id = id_generator(10)
+    def upload_image(self,imagename):
+        x = self.client.upload_from_path(imagename)
+        return x['link']
+
+
+imgur_client = ImgurAPIClient()
+# Generate an ID to be used as the image ID.
+def id_generator():
+    return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(10)])
+
+def parseAlbum(album_ID):
+    rand_id = id_generator()
     os.mkdir(rand_id)
-    for idx,i in enumerate(links):
-        testfile = urllib.URLopener()
-        testfile.retrieve(i,rand_id + '/' + str(idx).zfill(3) + '.gif')
+    imgur_client.get_album(album_ID,rand_id)
+
+    # concatenate the gifs into one single super-gif with the folder's name
     os.system('convert -delay 0 -loop 0 ' + rand_id + '/*.gif ' + rand_id + '.gif')
+
+    # upload the image to imgur
+    x = imgur_client.upload_image('./' + rand_id + '.gif')
+
+    # Clean up our created files
     shutil.rmtree(rand_id)
-    x = client.upload_from_path('./' + rand_id + '.gif', config=None, anon=True)
     os.remove(rand_id + '.gif')
-    return x['link']
+    return x
 
 if __name__=='__main__':
     print 'Input the album string or url:'
